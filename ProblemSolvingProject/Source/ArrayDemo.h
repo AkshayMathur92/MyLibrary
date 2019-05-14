58d2499597dc0fa6a863795c7ac3ea81d83e874e
Akshay Mathur <avi.mathur.engg+github@gmail.com>
2019-05-14 11:34:32 +0530

    Correcting Directory Structures for project

--- ProblemSolvingProject/Source/ArrayDemo.h
+++ ProblemSolvingProject/Source/ArrayDemo.h
@@ -8,4 +8,3 @@ public:
 	virtual ~ArrayDemo();
 	void run_demo();
 };
-
--- ProblemSolvingProject/Source/ProblemSolvingProject.cpp
+++ ProblemSolvingProject/Source/ProblemSolvingProject.cpp
@@ -1,5 +1,4 @@
 // ProblemSolvingProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 #include "Search.h"
